a39bc71de3d8f9e449c465f487cd0986ba96a2e9
azp69 <44404813+azp69@users.noreply.github.com>
2019-04-20 15:32:24 +0300

    tietokanta.php siivousta

--- modules/tietokanta.php
+++ modules/tietokanta.php
@@ -18,63 +18,7 @@
         {
 
         }
-
-        public function LisaaMokki($nimi, $kuvaus, $sijainti, $hinta)
-        {
-            $connection = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
-
-            if ($connection->connect_error)
-            {
-                die("Ei saada yhteyttä tietokantaan.");
-            }
-
-            $ni = mysqli_real_escape_string($connection, $nimi);
-            $ku = mysqli_real_escape_string($connection, $kuvaus);
-            $si = mysqli_real_escape_string($connection, $sijainti);
-            $hi = mysqli_real_escape_string($connection, $hinta);
-
-            $query = "INSERT INTO mokit (nimi, kuvaus, sijainti, hinta) VALUES ('$ni' ,'$ku', '$si', '$hi')";
-
-            if($connection->query($query) === TRUE)
-            {
-                echo "OK.";
-            }
-            else
-            {
-                echo "Pieleen meni.";
-            }
-
-            $connection->close();
-        
-        }
-
-        public function ListaaMokit()
-        {
-            $connection = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
-
-            if ($connection->connect_error)
-            {
-                die("Ei saada yhteyttä tietokantaan.");
-            }
-
-            $query = "SELECT * FROM mokit";
-
-            $result = $connection->query($query);
-
-            if ($result->num_rows > 0) 
-            {
-                while($row = $result->fetch_assoc()) {
-                    echo "ID: " . $row["id"] . ". Nimi: " . $row["nimi"] . ". Kuvaus: " . $row["kuvaus"] . ". Sijainti: " . $row["sijainti"] . ". Hinta: " . $row["hinta"] . ".</br>";
-                }
-            } 
-        
-            else 
-            {
-                echo "Ei yhtään tulosta.";
-            }
-            $connection->close();
-        }
-
+      
         public function KirjauduSisaan($tunnus, $salasana)
         {
             $connection = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
@@ -111,7 +55,10 @@
             }
             $connection->close();
         }
-        
+
+        /*
+            Palauttaa mökin (palvelun) varaukset taulukossa, eli siis varauksen aloituspvm ja lopetuspvm
+        */
         public function HaePalvelunVarauskalenteri($palveluId)
         {
             $connection = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
@@ -147,6 +94,10 @@
             return $varauskalenteri;
         }
 
+
+        /*
+            Etsii asiakasta etu- ja/tai sukunimen perusteella ja palauttaa taulukon Asiakas-objekteista
+        */
         public function HaeAsiakkaat($hakusana)
         {
             $connection = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
